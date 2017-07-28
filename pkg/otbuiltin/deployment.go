0f93b1adea941a77ac54ea77685f47e4c93ecbec
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-07-28 14:56:00 +0200

    deployment: Implement a subset of the getters

--- pkg/otbuiltin/deployment.go
+++ pkg/otbuiltin/deployment.go
@@ -34,3 +34,24 @@ func (d *Deployment) native() *C.OstreeDeployment {
 	}
 	return (*C.OstreeDeployment)(unsafe.Pointer(d.GObject))
 }
+
+func (d *Deployment) Osname() string {
+	return C.GoString(C.ostree_deployment_get_osname(d.native()))
+}
+
+func (d *Deployment) Csum() string {
+	return C.GoString(C.ostree_deployment_get_csum(d.native()))
+}
+
+func (d *Deployment) Deployserial() int {
+	return int(C.ostree_deployment_get_deployserial(d.native()))
+}
+
+func (d *Deployment) OriginRelpath() string {
+	cstr := C.ostree_deployment_get_origin_relpath(d.native())
+
+	str := C.GoString(cstr)
+	C.free(unsafe.Pointer(cstr))
+
+	return str
+}
